78b36988f5eb24f97b58c15547c9ac5bec032526
Sowndarya <sowndarya1024@gmail.com>
2025-11-10 09:50:43 +0800

    Fixing App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.jsx
+import React from 'react';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import About from './Components/About';
+import Portfolio from './Components/Portfolio';
+import Experiences from './Components/Services';
+import Certifications from './Components/Contact';
+import Footer from './Components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div id="home" style={{ marginTop: '80px' }}></div>
+      <Home />
+      <About />
+      <Portfolio />
+      <Experiences />
+      <Certifications />
+      <Footer />
     </div>
   );
 }
